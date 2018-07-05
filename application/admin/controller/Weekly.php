ffdbcdb825c7706f3e50b81f4f727611f45c6008
daipianpian <daipianpian@outlook.com>
2018-07-05 19:28:47 +0800

    编辑周报

--- application/admin/controller/Weekly.php
+++ application/admin/controller/Weekly.php
@@ -9,19 +9,58 @@ class Weekly
        	return '周报列表';
     }
     public function getWeekly(){
-    	$data = db('weekly')->where('state',1)->order('id desc')->select();
-        $dataCount = count($data);
-        for($i=0; $i < $dataCount; $i++){
-            $admin['id'] = $data[$i]['adminId'];
-            $admin['state'] = '1';
-            $data[$i]['adminName'] = db('admin')->where($admin)->value('name');
-            $data[$i]['weeklyDatil'] = '点击即可查看详情';
+        $request = Request::instance();
+        // 是否为 POST 请求
+        if ($request->isPost()){
+        	$data = db('weekly')->where('state',1)->order('id desc')->select();
+            $dataCount = count($data);
+            for($i=0; $i < $dataCount; $i++){
+                $admin['id'] = $data[$i]['adminId'];
+                $admin['state'] = '1';
+                $data[$i]['adminName'] = db('admin')->where($admin)->value('name');
+                $data[$i]['weeklyDatil'] = '点击即可查看详情';
+            }
+        	$callback['code'] = 10000;
+        	$callback['data'] =  $data;
+        	$callback['message'] = '成功';
+        	echo json($callback)->getcontent();
         }
-    	$callback['code'] = 10000;
-    	$callback['data'] =  $data;
-    	$callback['message'] = '成功';
-    	echo json($callback)->getcontent();
     }
+
+    public function updateWeekly(){
+        $request = Request::instance();
+        // 是否为 POST 请求
+        if ($request->isPost()){
+            $postParams = $request->post();
+
+            $id = $postParams['id'];
+            $thisWeekWork = $postParams['thisWeekWork'];
+            $nextWeekWork = $postParams['nextWeekWork'];
+            $collaboration = $postParams['collaboration'];
+            $curtime = date('Y-m-d H:i:s');
+
+            $data = [
+                'thisWeekWork' => $thisWeekWork,
+                'nextWeekWork' => $nextWeekWork,
+                'collaboration' => $collaboration,
+                'update_time' => $curtime
+            ];     
+
+            $result = db('weekly')->where('id', $id)->update($data);
+
+            if($result){
+                $callback['status'] = '1';
+                $callback['message'] = '成功';
+                echo json($callback)->getcontent();
+            }else{
+                $callback['status'] = '0';
+                $callback['message'] = '失败';
+                echo json($callback)->getcontent();
+            }
+
+        }
+    }
+
     public function deleteWeekly(){
         $request = Request::instance();
         // 是否为 POST 请求
@@ -48,6 +87,7 @@ class Weekly
         }
     }
 
+
     public function getWeeklyDatil(){
         $request = Request::instance();
         // 是否为 POST 请求
@@ -60,6 +100,12 @@ class Weekly
             $condition['state'] = '1';
 
             $result = db('weekly')->where($condition)->find();
+
+
+            $admin['id'] = $result['adminId'];
+            $admin['state'] = '1';
+            $result['adminName'] = db('admin')->where($admin)->value('name');
+
 
             if($result){
                 $callback['status'] = '1';
